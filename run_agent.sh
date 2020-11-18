6b5cb40eaa4c60fafd036c39ed9556f196abc915
DoNotUpdateThis UseLocalConfig <DoNotUpdateThis@UseLocalConfig.com>
2020-11-18 01:26:13 -0500

    Updated run_agent.

--- run_agent.sh
+++ run_agent.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+export CARLA_ROOT=/home/stars/Code/carla_0.9.9             # change to where you installed CARLA
+export PORT=2000                                                    # change to port that CARLA is running on
+export ROUTES=/home/stars/Code/learning_based/leaderboard/data/routes/route_19.xml  # change to desired route
+export TEAM_AGENT=image_agent.py                                     # no need to change
+export TEAM_CONFIG=model.ckpt
+export HAS_DISPLAY=1
+#export TEAM_CONFIG=sample_data                                      # change path to save data
+
 export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla
-export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.8-py3.5-linux-x86_64.egg           # 0.9.8
+export PYTHONPATH=$PYTHONPATH:$CARLA_ROOT/PythonAPI/carla/dist/carla-0.9.9-py3.7-linux-x86_64.egg           # 0.9.8
 export PYTHONPATH=$PYTHONPATH:leaderboard
 export PYTHONPATH=$PYTHONPATH:leaderboard/team_code
 export PYTHONPATH=$PYTHONPATH:scenario_runner
@@ -11,8 +20,9 @@ else
     CHECKPOINT_ENDPOINT="$(dirname $TEAM_CONFIG)/$(basename $ROUTES .xml).txt"
 fi
 
-python3 leaderboard/leaderboard/leaderboard_evaluator.py \
---track=SENSORS \
+python leaderboard/leaderboard/leaderboard_evaluator.py \
+--challenge-mode \
+--track=dev_track_3 \
 --scenarios=leaderboard/data/all_towns_traffic_scenarios_public.json  \
 --agent=${TEAM_AGENT} \
 --agent-config=${TEAM_CONFIG} \
